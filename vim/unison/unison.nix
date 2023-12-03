816ff78c3da0f90b07eafd174a386c8f885cf14a
James Earl Douglas <james@earldouglas.com>
2023-12-03 07:06:21 -0700

    unison-nix: update to latest revision

--- vim/unison/unison.nix
+++ vim/unison/unison.nix
@@ -3,7 +3,7 @@ let src =
   pkgs.fetchFromGitHub {
     owner = "ceedubs";
     repo = "unison-nix";
-    rev = "378814b43702c78b8daca0e6f9444e73cb6c9955";
-    sha256 = "sha256:0zk3cjxcz41nf0nfqj3nss9j8r9qns90a0r2kgpyf4qf3slzz6bq";
+    rev = "d0c99d76a5beea2c0a829f0c5915d52b5341141e";
+    sha256 = "1sirbbbnxg1b63vfdivv27vxd9lwh9yddg8qwwcgj8i5d3gzqa1q";
   };
 in import "${src}/default.nix" {}
